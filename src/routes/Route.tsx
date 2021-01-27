2634881aa1effead4567599960708f605b65155a
dufellipeR <eduardo.fellipe30@gmail.com>
2021-01-26 22:08:55 -0300

    Addin admin authorization

--- src/routes/Route.tsx
+++ src/routes/Route.tsx
@@ -8,11 +8,13 @@ import { useAuth } from '../hooks/auth';
 
 interface RouteProps extends ReactRouterProps {
   isPrivate?: boolean;
+  isAdmin?: boolean;
   component: React.ComponentType;
 }
 
 const Route: React.FC<RouteProps> = ({
   isPrivate = false,
+  isAdmin = false,
   component: Component,
   ...rest
 }) => {
@@ -22,12 +24,24 @@ const Route: React.FC<RouteProps> = ({
     <ReactRoute
       {...rest}
       render={({ location }) => {
-        return isPrivate === !!user ? (
+        if (!isAdmin) {
+          return isPrivate === !!user ? (
+            <Component />
+          ) : (
+            <Redirect
+              to={{
+                pathname: isPrivate ? '/' : '/dashboard',
+                state: { from: location },
+              }}
+            />
+          );
+        }
+        return isAdmin === !!user?.isAdmin ? (
           <Component />
         ) : (
           <Redirect
             to={{
-              pathname: isPrivate ? '/' : '/dashboard',
+              pathname: isAdmin ? '/admin' : '/admin/dashboard',
               state: { from: location },
             }}
           />
