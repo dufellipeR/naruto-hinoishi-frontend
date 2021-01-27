f28a39325bf34e557f4d49e68c1a658dac34fe94
dufellipeR <eduardo.fellipe30@gmail.com>
2021-01-26 22:07:09 -0300

    Adding Select component

---
+++ src/components/Select/styles.ts
@@ -0,0 +1,77 @@
+import styled, { css } from 'styled-components';
+import Tooltip from '../Tooltip';
+
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+  isErrored: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
+  background: white;
+  border-radius: 10px;
+  border: 2px solid white;
+  padding: 16px;
+  width: 100%;
+  color: #666360;
+  display: flex;
+  align-items: center;
+
+  & + div {
+    margin-top: 8px;
+  }
+
+  ${(props) =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+    `}
+
+  ${(props) =>
+    props.isFocused &&
+    css`
+      color: #711a19;
+      border-color: #711a19;
+    `}
+
+  ${(props) =>
+    props.isFilled &&
+    css`
+      color: #711a19;
+    `}
+
+
+
+  input {
+    flex: 1;
+    background: transparent;
+    border: 0;
+    color: #666360;
+
+    &::placeholder {
+      color: #666360;
+    }
+  }
+
+  svg {
+    margin-right: 16px;
+  }
+`;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #fff;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
+  }
+`;
